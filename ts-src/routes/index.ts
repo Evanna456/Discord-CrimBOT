691e6f961a65a24b7b30ccfe9ff99ae8810e0fd2
Evanna Tabs <91871021+Evanna456@users.noreply.github.com>
2022-05-24 20:37:05 +0800

    Delete ts-src directory

--- ts-src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-var express:any = require('express');
-var router:any = express.Router();
-
-/* GET home page. */
-router.get('/', function (req:any, res:any, next:any) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-module.exports = router;
