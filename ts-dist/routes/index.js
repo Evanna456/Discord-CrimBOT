7ea96c465b9afd030fe2a85006e921282883d814
Evanna Tabs <91871021+Evanna456@users.noreply.github.com>
2022-05-24 20:36:55 +0800

    Delete ts-dist directory

--- ts-dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var express = require('express');
-var router = express.Router();
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-module.exports = router;
